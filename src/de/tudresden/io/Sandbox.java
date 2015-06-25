4da233ece48baaa96d991d1c6eb7e7e1adbb592d
Rodrigo Lins <lins.oliveira@gmail.com>
2015-06-25 22:47:41 +0200

    Initial commit

---
+++ src/de/tudresden/io/Sandbox.java
@@ -0,0 +1,29 @@
+package de.tudresden.io;
+
+import java.io.InputStream;
+
+import com.google.gson.Gson;
+
+public class Sandbox {
+
+	public static void main(String[] args) {
+		final String TEST = "{\"type\":\"temperature\",\"model\":\"LM35\",\"temperature\":22.95,\"unit\":\"celcilus\"}" +
+//			"Doing something" +
+			"{\"type\":\"push_button\",\"model\":\"generic\",\"state\":\"off\"}" +
+//			"Doing something" +
+			"{\"type\":\"hygrometer\",\"model\":\"DHT22\",\"humidity\":55.60,\"humidityUnit\":\"%\",\"temperature\":23.50,\"temperatureUnit\":\"celcius\",\"heatIndex\":25.12,\"heatIndexUnit\":\"celcius\"}" +
+//			"Doing something" +
+			"{\"type\":\"triaxial_accelerometer\",\"model\":\"MMA7361\",\"xAxis\":-4.67,\"xAxisUnit\":\"g\",\"yAxis\":-5.00,\"yAxisUnit\":\"g\",\"zAxis\":-5.00,\"zAxisUnit\":\"g\"}" +
+//			"Doing something" +
+			"{\"type\":\"motion\",\"model\":\"RE200B\",\"motion\":\"false\"}" +
+//			"Doing something" +
+			"{\"type\":\"ultrasonic\",\"model\":\"HC-SR04\",\"distance\":6,\"unit\":\"cm\"}";
+//			"Doing something" +
+//			"echo: echo echo!echo echo!echo echo!echo echo!echo echo!e";
+		final String TEST2 = "{\"id\":5,\"type\":\"motion\",\"model\":\"RE200B\",\"properties\":{\"state\":{\"value\":\"true\",\"unit\":\"binary\"}}}";
+		Gson gson = new Gson();
+		gson.fromJson(TEST2, classOfT)
+		
+
+	}
+}
---
+++ src/de/tudresden/io/TwoWaySerialComm.java
@@ -0,0 +1,106 @@
+package de.tudresden.io;
+
+import gnu.io.CommPort;
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TwoWaySerialComm {
+
+	private final static int TIMEOUT = 2000;
+	private final static int DATA_RATE = 9600;
+	
+	void connect(String portName) throws Exception {
+		System.setProperty("gnu.io.rxtx.SerialPorts", "/dev/ttyACM1");
+		
+		CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
+		if (portIdentifier.isCurrentlyOwned()) {
+			System.out.println("Error: Port is currently in use");
+		} else {
+			int timeout = TIMEOUT;
+			CommPort commPort = portIdentifier.open(this.getClass().getName(), timeout);
+
+			if (commPort instanceof SerialPort) {
+				SerialPort serialPort = (SerialPort) commPort;
+				serialPort.setSerialPortParams(DATA_RATE, SerialPort.DATABITS_8,
+						SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+
+				InputStream in = serialPort.getInputStream();
+				OutputStream out = serialPort.getOutputStream();
+
+				(new Thread(new SerialReader(in))).start();
+				(new Thread(new SerialWriter(out))).start();
+
+			} else {
+				System.out.println("Error: Only serial ports are handled by this example.");
+			}
+		}
+	}
+
+	public static class SerialReader implements Runnable {
+
+		InputStream in;
+		List<String> queue = new ArrayList<String>();
+
+		public SerialReader(InputStream in) {
+			this.in = in;
+		}
+
+		public void run() {
+			byte[] buffer = new byte[1024];
+			int len = -1;
+			try {
+				while ((len = this.in.read(buffer)) > -1) {
+					System.out.print(new String(buffer, 0, len));
+					queue.add(new String(buffer, 0, len));
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		public List<String> getQueueAndErase(){
+			List<String> newQueue = queue;
+			queue = new ArrayList<String>();
+			return newQueue;
+		}
+	}
+
+	public static class SerialWriter implements Runnable {
+
+		OutputStream out;
+
+		public SerialWriter(OutputStream out) {
+			this.out = out;
+		}
+
+		public void run() {
+			try {
+				int c = 0;
+				while ((c = System.in.read()) > -1) {
+					this.out.write(c);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public class Observer {
+		String value = "";
+	}
+
+	public static void main(String[] args) {
+		try {
+			TwoWaySerialComm serial = new TwoWaySerialComm();
+			serial.connect("/dev/ttyACM1");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
